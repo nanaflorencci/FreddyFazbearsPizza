ac652a5045721546a08409d87b682daf4688febd
nanaflorencci <giovannaflorencci1814@gmail.com>
2024-03-21 13:14:32 -0300

    .

--- src/FlatListExample.tsx
+++ src/FlatListExample.tsx
@@ -1,48 +1,57 @@
 import React from "react";
-import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, StatusBar, StyleSheet, Text, TextInput, TextInputBase, TouchableOpacity, View } from "react-native";
 
-interface menuItem{
+interface menuItem {
     id: string;
     nome: string;
+    preco: number;
     ingredientes: string;
-    preco: string;
-    image: any
+    image: any;
 }
 
 const dados: menuItem[] = [
-    {id: '1', nome: 'Yakisoba (Sessão da culinária japonesa)', ingredientes: 'Macarrão, carne, molho tarê, cenoura, brocolis.', preco:  "R$55.00", image: require('../src/assets/images/menu.png')},
-    {id: "2",nome: "Pizza (Sessão da culinária italiana)", ingredientes: 'Massa, molho; queijo; peperone; manjericão.', preco:  "R$35.00", image: require('../src/assets/images/menu.png')},
-    {id: "3",nome: "Babaganoush (Sessão da culinária árabe)",  ingredientes: 'Berinjelas, pasta de gergelim, azeite, alho.', preco:  "R$25.00", image: require('../src/assets/images/menu.png')},
-    {id: "4",nome: "Feijoada (Sessão da culinária brasileira)",  ingredientes: 'Feijão preto, carne, linguiça, alho, cebolinha.', preco:  "R$15.00", image: require('../src/assets/images/menu.png')},
-    {id: "5",nome: "Topokki (Sessão da culinária coreana)",  ingredientes: 'Bolinho de arroz, cebola, dashi, peixe, ovos cozidos.', preco:  "R$20.00", image: require('../src/assets/images/menu.png')},
-    {id: "6",nome: "Taco (Sessão da culinária mexicana)",  ingredientes: 'Tortilha, guacamole, chili, pimenta, tomate.', preco:  "R$15.00", image: require('../src/assets/images/menu.png')},
-    {id: "7",nome: "Mac N Cheese (Sessão da culinária canadense)",  ingredientes: 'Macarrão, leite, queijo, alho e bacon.', preco:  "R$15.00", image: require('../src/assets/images/menu.png')},
-    {id: "8",nome: "Chucrute (Sessão da culinária alemã)",  ingredientes: 'Repolho, vinagre, pimenta, gengibre, azeite.', preco:  "R$25.00", image: require('../src/assets/images/menu.png')},
-    {id: "9",nome: "Coq au Vin (Sessão da culinária francesa)",  ingredientes: 'Frango, pimenta, alho, folha de Louro, cenoura.', preco:  "R$45.00", image: require('../src/assets/images/menu.png')},
-    {id: "10",nome: "Hambúrguer (Sessão da culinária estadunidense)",  ingredientes: 'Pão, hambúrguer, queijo, alface, tomate.', preco:  "R$25.00", image: require('../src/assets/images/menu.png')},
-    {id: "11",nome: "Butter Chicken (Sessão da culinária indiana)",  ingredientes: 'Franfo, pimenta, páprica, cúrcuma, cominho.', preco:  "R$40.00", image: require('../src/assets/images/menu.png')},
-    {id: "12",nome: "Bacalhau à brás (Sessão da culinária portuguesa)",  ingredientes: 'Bacalhau, batata, ovos, cebola, pimenta, salsa.', preco:  "R$40.00", image: require('../src/assets/images/menu.png')},
-    {id: "13",nome: "Asado (Sessão da culinária argentina)",  ingredientes: 'Carne, chimichurri, erva, alho, vinagre.', preco:  "R$40.00", image: require('../src/assets/images/menu.png')},
-    {id: "14",nome: "Zong Zi (Sessão da culinária chinesa)",  ingredientes: 'Carne, cogumelos, castanhas, feijão, ovos.', preco:  "R$40.00", image: require('../src/assets/images/menu.png')},
-    {id: "15",nome: "Ceviche (Sessão da culinária peruana)",  ingredientes: 'Tilápia, pasta de alho, tomate, pimenta, cebola.', preco:  "R$40.00", image: require('../src/assets/images/menu.png')},
-
+    {id: '1', nome: '𝒴𝒶𝓀𝒾𝓈𝑜𝒷𝒶', ingredientes: 'Massa, carne, molho tarê, cenoura e brocolis.', preco:  55.00, image: require('../src/assets/images/yakisoba.png')},
+    {id: "2",nome: "𝒫𝒾𝓏𝓏𝒶", ingredientes: 'Massa, molho, queijo, peperone e manjericão.', preco:  35.00, image: require('../src/assets/images/pizza.png')},
+    {id: "3",nome: "𝐵𝒶𝒷𝒶𝑔𝒶𝓃𝑜𝓊𝓈𝒽",  ingredientes: 'Berinjelas, pasta de gergelim, azeite e alho.', preco:  25.00, image: require('../src/assets/images/babaganoush.png')},
+    {id: "4",nome: "𝐹𝑒𝒾𝒿𝑜𝒶𝒹𝒶",  ingredientes: 'Feijão preto, carne, linguiça, alho e cebolinha.', preco:  15.00, image: require('../src/assets/images/feijoada.png')},
+    {id: "5",nome: "𝒯𝑜𝓅𝑜𝓀𝓀𝒾",  ingredientes: 'Bolinho de arroz, cebola, dashi, peixe e ovos.', preco:  20.00, image: require('../src/assets/images/topokki.png')},
+    {id: "6",nome: "𝒯𝒶𝒸𝑜",  ingredientes: 'Tortilha, guacamole, chili, pimenta, tomate.', preco:  15.00, image: require('../src/assets/images/taco.png')},
+    {id: "7",nome: "𝑀𝒶𝒸 𝒩 𝒞𝒽𝑒𝑒𝓈𝑒",  ingredientes: 'Macarrão, leite, queijo, alho e bacon.', preco:  15.00, image: require('../src/assets/images/macandcheese.png')},
+    {id: "8",nome: "𝐿𝒶𝓂𝑒𝓃",  ingredientes: 'Macarrão, frango, cebolinha, alga e ovos.', preco:  25.00, image: require('../src/assets/images/lamen.png')},
+    {id: "9",nome: "𝒞𝑜𝓆 𝒜𝓊 𝒱𝒾𝓃",  ingredientes: 'Frango, pimenta, alho, cebolinha e cenoura.', preco:  45.00, image: require('../src/assets/images/coqauvin.png')},
+    {id: "10",nome: "𝐻𝒶𝓂𝒷𝓊𝓇𝑔𝓊𝑒𝓇",  ingredientes: 'Pão, hambúrguer, queijo, alface e tomate.', preco:  25.00, image: require('../src/assets/images/hamburguer.png')},
+    {id: "11",nome: "𝐵𝓊𝓉𝓉𝑒𝓇 𝒞𝒽𝒾𝒸𝓀𝑒𝓃",  ingredientes: 'Frango, pimenta, páprica, cúrcuma e cominho.', preco:  40.00, image: require('../src/assets/images/butterchicken.png')},
+    {id: "12",nome: "𝐵𝒶𝒸𝒶𝓁𝒽𝒶𝓊 𝒶 𝐵𝓇𝒶𝓈",  ingredientes: 'Bacalhau, batata, ovos, cebola e pimenta.', preco:  40.00, image: require('../src/assets/images/bacalhauabras.png')},
+    {id: "13",nome: "𝒜𝓈𝒶𝒹𝑜",  ingredientes: 'Carne, chimichurri, erva, alho e vinagre.', preco:  40.00, image: require('../src/assets/images/asado.png')},
+    {id: "14",nome: "𝒵𝑜𝓃𝑔 𝒵𝒾",  ingredientes: 'Carne, cogumelos, castanhas, feijão e ovos.', preco:  40.00, image: require('../src/assets/images/zongzi.png')},
+    {id: "15",nome: "𝒞𝑒𝓋𝒾𝒸𝒽𝑒",  ingredientes: 'Tilápia, pasta de alho, pimenta e cebola.', preco:  40.00, image: require('../src/assets/images/ceviche.png')}
 ];
 
-const renderItem = ({item}: {item: menuItem}) => (
-    <View style={styles.item}>
-        <Text>{item.nome}</Text>
-        <Text>{item.preco}</Text>
-        <Text>{item.ingredientes}</Text>
-    </View>
+const renderItem = ({ item }: { item: menuItem }) => (
+    <TouchableOpacity style={styles.item}>
+        <Text style={styles.itemTitle}>{item.nome}</Text>
+        <Text style={styles.linhas}>◇───────────────◇───────────────◇</Text>
+        <Text style={styles.textIngredientes}>{item.ingredientes}</Text>
+        <Image source={item.image} style={styles.image} />
+        <Text style={styles.textPreco}>R${item.preco},00</Text>
+    </TouchableOpacity>
 );
 
-function FlatListExample(): React.JSX.Element{
-    return(
+function FlatListExample(): React.JSX.Element {
+    return (
         <View style={styles.container}>
-            <StatusBar backgroundColor='#000000' barStyle='light-content' />
+            <StatusBar backgroundColor="black" barStyle='light-content' />
+
             <View style={styles.header}>
-                <Text style={styles.headerText}>✰Taste Of World✰</Text>
+                <Text style={styles.headerText}>✰𝕿𝖆𝖘𝖙𝖊 𝕺𝖋 𝖂𝖔𝖗𝖑𝖉✰</Text>
             </View>
+
+            <Text style={styles.linhaTitle}>◎━━━━━━━━━━━━━━━━◎.◈.◎━━━━━━━━━━━━━━━━━◎</Text>
+
+            <View>
+                <TextInput style={styles.pesquisar} placeholder="Pesquisar"></TextInput>
+            </View>
+
             <FlatList
                 showsVerticalScrollIndicator={false}
                 data={dados}
@@ -52,11 +61,11 @@ function FlatListExample(): React.JSX.Element{
 
             <View style={styles.footer}>
                 <TouchableOpacity>
-                    <Image source={require('./assets/images/home.png')} style={styles.footerIcon} />
+                    <Image source={require('./assets/images/home.webp')} style={styles.footerIcon} />
                 </TouchableOpacity>
 
                 <TouchableOpacity>
-                <Image source={require('./assets/images/pedidos.png')} style={styles.footerIcon}/>
+                <Image source={require('./assets/images/pedidos.webp')} style={styles.footerIcon}/>
                 </TouchableOpacity>
 
                 <TouchableOpacity>
@@ -66,46 +75,103 @@ function FlatListExample(): React.JSX.Element{
                 <TouchableOpacity>
                 <Image source={require('./assets/images/menu.png')} style={styles.footerIcon}/>
                 </TouchableOpacity>
-            </View>
 
+                <TouchableOpacity>
+                <Image source={require('./assets/images/carrinho.webp')} style={styles.footerIcon}/>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    item: {
-        backgroundColor: '#edf6f9',
-        padding: 10,
-        marginVertical: 10,
-        marginHorizontal: 10,
-        borderRadius: 15,
-        borderWidth: 10,
-        borderColor: '#000000'
-    },
     container: {
-        flex: 1,
+        backgroundColor: '#000000' ,
+        flex: 1
+
+    },
+    item: {
+        backgroundColor: 'black',
+        padding: 20,
+        marginVertical: 10,
+        borderRadius: 30,
+        borderColor: '#FFF',
+        borderWidth: 3
+    },
+    itemText: {
+        color: '#FFF',
+        flexDirection: 'column',
+        fontSize:20,
+        marginTop: 3
+    },
+    itemTitle:{
+        fontSize: 30,
+        color:'#FFF',
+        marginTop: 3
     },
     header: {
-        backgroundColor: '#000000',
+        backgroundColor: "#000000",
+        flexDirection: 'row',
+        height: 100,
         alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 10,
+        borderColor: '#FFF',
+        borderRadius: 30
     },
     headerText: {
-        fontSize: 40,
-        fontWeight: '100',
-        color: 'white'
+        fontSize: 30,
+        color: '#FFF',
+        marginLeft: 65
     },
     footer: {
         borderTopWidth: 0.2,
-        backgroundColor: 'white',
-        flexDirection: 'row',
+        backgroundColor: '#FFF',
+        flexDirection: "row",
         justifyContent: 'space-around',
         alignItems: 'center',
         paddingVertical: 10
     },
     footerIcon: {
-        width: 30,
-        height: 30
+        height: 30,
+        width: 30
+    },
+    image: {
+        height: 100,
+        width: 170,
+        borderRadius: 10,
+        borderWidth: 3,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 10
+    },
+    linhas:{
+        color:'#FFF'
+    },
+    textPreco: {
+        color: '#FFF',
+        flexDirection: 'column',
+        fontSize:15,
+        marginTop: 3,
+        fontStyle: 'italic'
+    },
+    textIngredientes: {
+        color: '#FFF',
+        flexDirection: 'column',
+        fontSize:17,
+        marginTop: 3,
+        fontStyle: 'italic',
+        paddingVertical: 5,
+    },
+    pesquisar: {
+        backgroundColor: '#FFF',
+        padding: 10,
+        marginVertical: 30,
+        borderRadius: 30,
+        borderColor: '#FFF',
+        borderWidth: 3,
+    },
+    linhaTitle: {
+        color:'#FFF'
     }
 });
 
